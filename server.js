7655be15d4c2918f1f383e83c8335b0b5eab0009
Woner13oy <wandilenxumalo131@gmail.com>
2019-07-01 15:54:20 +0200

    Page now showing

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const pug = require('pug');
 
+//Load View Engine
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//load static files
+app.use(express.static('views'));
+
+//Weather Route
 app.get('/weather', (req, res) => {
-    res.render('index');
-})
+	res.render('index');
+});
 
 app.listen(8880, () => {
-    console.log('Server Running');
-})
+	console.log('Server Running');
+});
